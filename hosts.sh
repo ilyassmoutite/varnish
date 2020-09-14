4aad23bd4bb36e9f9516cbf394401a07cb91077e
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2020-09-14 13:25:02 +0100

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,5 +1,5 @@
 echo "[varnish]" >> /etc/ansible/hosts
-for line in $(cat text); do echo "$line ansible_ssh_user=centos ansible_ssh_pass=centos">> /etc/ansible/hosts ; done 
+for line in $(cat /home/centos/varnish-ip); do echo "$line ansible_ssh_user=centos ansible_ssh_pass=centos">> /etc/ansible/hosts ; done 
 
 echo "[app]" >> /etc/ansible/hosts
-for line in $(cat text); do echo "$line ansible_ssh_user=centos ansible_ssh_pass=centos">> /etc/ansible/hosts ; done
+for line in $(cat /home/centos/magento-ip); do echo "$line ansible_ssh_user=centos ansible_ssh_pass=centos">> /etc/ansible/hosts ; done
