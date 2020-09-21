317738829ec826357a529b8834620f10f16957d7
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2020-09-21 13:39:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 
-rm /etc/ansible/hosts /home/centos/varnish-ip /home/centos/magento-ip script.sh
+rm /etc/ansible/hosts /home/centos/varnish/varnish-ip /home/centos/varnish/magento-ip script.sh
 touch /home/centos/varnish-ip
 touch /home/centos/magento-ip
 
 touch /etc/ansible/hosts
 echo $(aws ec2 describe-instances --region eu-west-1 --query 'Reservations[*].Instances[*].[PublicIpAddress]' --filters \
-"Name=instance-state-name,Values=running"  'Name=tag:Name,Values=VarnishProduction' --output text ) >> /home/centos/varnish-ip
+"Name=instance-state-name,Values=running"  'Name=tag:Name,Values=VarnishProduction' --output text ) >> /home/centos/varnish/varnish-ip
 
 echo $(aws ec2 describe-instances --region eu-west-1 --query 'Reservations[*].Instances[*].[PublicIpAddress]' --filters \
-"Name=instance-state-name,Values=running"  'Name=tag:Name,Values=HelloWorld' --output text ) >> /home/centos/magento-ip
-chmod 777 /home/centos/varnish-ip
-chmod 777 /home/centos/magento-ip
+"Name=instance-state-name,Values=running"  'Name=tag:Name,Values=HelloWorld' --output text ) >> /home/centos/varnish/magento-ip
+chmod 777 /home/centos/varnish/varnish-ip
+chmod 777 /home/centos/varnish/magento-ip
 cd varnish
 export ANSIBLE_HOST_KEY_CHECKING=False
 aws s3 cp s3://key-ilyass/magentokey_eu_west_1.pem key.pem
